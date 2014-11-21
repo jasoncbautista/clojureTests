2cd17d568f0be7242309ba3b935ec8c7214168bb
Jason <bjason@gmail.com>
2014-11-20 18:33:49 -0800

    compile shit

--- simpleapp2/src/simpleapp2/core.clj
+++ simpleapp2/src/simpleapp2/core.clj
@@ -1,6 +1,25 @@
 (ns simpleapp2.core
   (:gen-class))
 
+
+
+
+(defn multiArr
+  ([] (println "jason"))
+
+  ([lastName] (println "Jason" lastName))
+
+  ([lastName & more] (println "Jason")  (apply multiArr more) )
+)
+(defn print10
+
+  "some ismple fxn to print 10"
+
+  []
+  (println 10)
+
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
